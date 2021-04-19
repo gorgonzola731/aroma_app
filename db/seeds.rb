6c9523c5f2a1026c97be9616f44ed79fa17baae9
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-19 20:42:08 +0900

    Userモデルにnameカラム追加

--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-NAME = 'hogehoge'
-EMAIL = 'test@example.com'
-PASSWORD = 'password'
-
-User.find_or_create_by!(email: EMAIL) do |user|
-  user.password = PASSWORD
-  puts 'ユーザーの初期データインポートに成功しました。'
-end 
