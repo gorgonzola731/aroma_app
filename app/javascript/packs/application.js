e74809dd85a18e2197ffb27474acd8ab7944bcbb
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-19 17:20:31 +0900

    bootstrap下のjavascriptを動作させる、トップページの作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,26 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+$(document).on('turbolinks:load', function () {
+  function filterTableRows() {
+      const value = $(this).val().toLowerCase();
+      $("#language-tbody tr").filter(function () {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  }
+
+  $("#language-search").on("input", filterTableRows);
+});
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../../assets/stylesheets/application';
+import "bootstrap/dist/js/bootstrap"
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import '../stylesheets/application';
-import "bootstrap/dist/js/bootstrap"
+//= require jquery
+//= require jquery_ujs
