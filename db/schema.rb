bcd7390ae1e601d81b4064374750d8b2b16e196c
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-19 19:52:09 +0900

    Tipモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_104906) do
+ActiveRecord::Schema.define(version: 2021_04_19_105129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2021_04_19_104906) do
     t.text "content"
     t.string "image"
     t.string "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tips", force: :cascade do |t|
+    t.string "title"
+    t.text "article"
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
