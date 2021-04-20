dd0afd1babdf4d46208fcce8b969b6b8ef27fff1
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-20 16:14:01 +0900

    csvインポートでPostモデルのデータが受け取れるように設定

---
+++ lib/autoloads/import_csv.rb
@@ -0,0 +1,14 @@
+require "csv"
+
+class ImportCsv
+  def self.import(path)
+     CSV.foreach(path, headers: true) do |row|
+       Post.create!(
+         title: row["title"],
+         aroma: row["aroma"],
+         content: row["content"]
+         user_id: row["user_id"]
+       )
+     end
+  end
+end
