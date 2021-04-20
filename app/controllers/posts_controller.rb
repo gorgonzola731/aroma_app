1329be8221a6f425b267cc7a8ec28a7e42dbd8b7
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-20 11:53:04 +0900

    postsのルーティング設定とコントローラの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,38 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @posts = Post.all.order(created_at: :desc) 
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    current_user.posts.create!(post_params)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content, :image, :aroma)
+    end
+
 end
