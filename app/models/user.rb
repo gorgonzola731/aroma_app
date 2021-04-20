9b6849160a1a58d150edf3c558e8854863ac8162
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-20 16:26:09 +0900

    PostモデルとUserモデルのアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
