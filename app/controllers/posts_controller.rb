4a46264436402cee50746067fc300d964aceb9cd
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-22 18:25:57 +0900

    モーダルに改行を挿入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :content, :image, :aroma,).merge(user_id: current_user.id)
+      params.require(:post).permit(:title, :content, :image, :aroma).merge(user_id: current_user.id)
     end
 
 end
