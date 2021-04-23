5b411a6b70ebfa9b4a7f95bb284c2cdc9dfa426c
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-23 13:01:57 +0900

    ゲストログインボタンを配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(name:' guest_user',email: 'guest@example.com') do |user|
     user.password = SecureRandom.urlsafe_base64
     # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
