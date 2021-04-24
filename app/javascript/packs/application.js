771613cc9eb596374143b2d7e4a1390c658e45ef
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-24 19:21:39 +0900

    エラー修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,13 @@
+$(document).on('turbolinks:load', function () {
+  function filterTableRows() {
+      const value = $(this).val().toLowerCase();
+      $("#language-tbody tr").filter(function () {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  }
 
+  $("#language-search").on("input", filterTableRows);
+});
 window.$ = $
 
 import Rails from "@rails/ujs"
