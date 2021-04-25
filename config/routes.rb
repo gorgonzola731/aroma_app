e016d7b5514df6e3f2237a9ed698635948960996
gorgonzola731 <nk.kenken31@gmail.com>
2021-04-25 11:13:22 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   resources :posts do
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy],
+    resources :comments, only: [:create, :destroy]
   end
 end
