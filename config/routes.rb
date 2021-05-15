72f933f398dcf55cbdef33d267dce01354787063
gorgonzola731 <nk.kenken31@gmail.com>
2021-05-15 16:03:42 +0900

    homeコントローラにdiagnosesアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
   root to: 'home#index'
+  get 'diagnoses', to: 'home#diagnoses'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
