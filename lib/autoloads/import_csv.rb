2d80618f3321bbd01868a01bbef106094b8255a2
gorgonzola731 <nk.kenken31@gmail.com>
2021-05-16 16:09:47 +0900

    tipsテーブルに対するcsvインポート機能の実装

--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -1,14 +1,16 @@
 require "csv"
 
 class ImportCsv
-  def self.import(path)
-     CSV.foreach(path, headers: true) do |row|
-       Post.create!(
-         title: row["title"],
-         aroma: row["aroma"],
-         content: row["content"],
-         user_id: row["user_id"],
-       )
-     end
-  end
+  path = File.join Rails.root, "db/csv_data/tip_data.csv"
+    list = []
+    CSV.foreach(path, headers: true) do |row|
+      list << {
+          image: File.open("#{Rails.root}/#{row["image"]}"),
+          title: row["title"],
+          article: row["article"],
+      }
+    end
+    puts "インポート処理を開始"
+     Tip.create!(list)
+      puts "インポート完了!!"
 end
