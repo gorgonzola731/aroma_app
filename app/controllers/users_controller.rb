5bf49b111a85ae93f137f36e8b9143c7690c22a5
gorgonzola731 <nk.kenken31@gmail.com>
2021-05-29 18:01:13 +0900

    ユーザーページをタブを使って分ける

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(created_at: :desc)
+    @likes = Like.where(user_id: @user.id)
+    @post = Post.find(params[:id])
   end
 end
