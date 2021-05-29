915a4d14caf84bc19935c87b0b8d53647ef71c3a
gorgonzola731 <nk.kenken31@gmail.com>
2021-05-29 18:32:14 +0900

    エラー解消とインデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(created_at: :desc)
-    @likes = Like.where(user_id: @user.id)
+    @likes = Like.where(user_id: @user.id).order(created_at: :desc)
     @post = Post.find(params[:id])
   end
 end
