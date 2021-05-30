c0babe09cb51912e6858c550315b93f988fe9539
gorgonzola731 <nk.kenken31@gmail.com>
2021-05-30 17:45:42 +0900

    テストをリストアップする設定の記述

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,13 @@
 Rails.application.config.generators do |g|
   g.stylesheets false
   g.skip_routes true
+  g.assets false
   g.helper false
+  g.test_framework :rspec,
+                   fixtures: true,
+                   view_specs: false,
+                   routing_specs: false,
+                   helper_specs: false,
+                   controller_specs: false,
+                   request_specs: true
 end 
