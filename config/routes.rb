693941d7d0636214f5b6b7f39154a2ccb31448c5
gorgonzola731 <nk.kenken31@gmail.com>
2021-06-03 19:54:13 +0900

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :posts do
+  resources :posts , except: [:update, :edit ]do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
