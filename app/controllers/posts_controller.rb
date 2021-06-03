48db840f427094232b2c73baaaab5a6450c1c453
gorgonzola731 <nk.kenken31@gmail.com>
2021-06-03 19:47:30 +0900

    postコントローラ内の不要なメソッドを削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,15 +27,6 @@ class PostsController < ApplicationController
     @posts = Post.all.order(created_at: :desc) 
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-  end
-
   def destroy
     @post.destroy!
   end
