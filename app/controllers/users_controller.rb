a894403d42c54899eb1385e6e9e740e548bf8650
gorgonzola731 <nk.kenken31@gmail.com>
2021-06-04 17:33:42 +0900

    ページネーションの変更をマイページの自身の投稿欄にも反映

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  PER_PAGE = 5
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(created_at: :desc)
+    @posts = @posts.page(params[:page]).per(PER_PAGE)
     @likes = Like.where(user_id: @user.id).order(created_at: :desc)
   end
 end
