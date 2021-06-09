af2efea5ee77cd5f9033277ab816f25853beaa3b
gorgonzola731 <nk.kenken31@gmail.com>
2021-06-09 16:09:25 +0900

    不要な変更をもとに戻す

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_module
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-set :pty, true
+#set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
