43ba2671c6bca82dcae8147fb59e6f7c83bda6a1
gorgonzola731 <nk.kenken31@gmail.com>
2021-06-10 20:56:07 +0900

    carrierwaveの設定変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,5 +7,6 @@ if Rails.env.production?
       region: "ap-northeast-1"
     }
     config.fog_directory = "aroma-app-pbuckert"
+    config.asset_host = "https://static.aromalife-app.com"
   end
 end
