8a9138fe2dd584cfba3138d9f9e16b0d8c2f6480
gorgonzola731 <nk.kenken31@gmail.com>
2021-06-11 11:15:15 +0900

    不要なファイルの削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,4 @@ ADMIN_PASSWORD = "password".freeze
 AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
   admin_user.password = ADMIN_PASSWORD
   admin_user.password_confirmation = ADMIN_PASSWORD
-  puts "管理者の初期データインポートに成功しました。"
 end
