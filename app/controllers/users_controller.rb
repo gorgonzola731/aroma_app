98ce95732d59f48b2a06d78e7b80b6a5ee1793ee
gorgonzola731 <nk.kenken31@gmail.com>
2021-06-20 21:35:31 +0900

    includesメソッドでクエリの量を減らす

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order(created_at: :desc)
-    @posts = @posts.page(params[:page]).per(PER_PAGE)
-    @like_posts = @user.like_posts.page(params[:page]).per(PER_PAGE)
+    @posts = @user.posts.includes([:likes, :comments]).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+    @like_posts = @user.like_posts.includes([:likes, :comments]).page(params[:page]).per(PER_PAGE)
   end
 end
